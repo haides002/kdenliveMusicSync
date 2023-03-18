fc3893e8ce52a77d42c02a9afd70f7d18a32dcbe
Linus Pre <preiss.linus@gmail.com>
2023-03-18 10:38:36 +0100

    made it more good

--- kdenliveMusic.py
+++ kdenliveMusic.py
@@ -1,27 +1,28 @@
 # variables you should assing your self
-BPM = 150
-FPS = 60
-OFFSET = 48
-MARKERTYPE = 0
-SONGLENGTH = 16654
+FPS = 60 # the fps of your project
+MARKERTYPE = 0 # customize the type of marker
+
+BPM = 150 # pretty self explanetory
+OFFSET = 0 # how many frames until the song starts
+SONGLENGTH = 16654 # length of the song in frames 
+
 
 # variables that are dynamic (do not change them)
 beatDuration = 60 / BPM
 barDuration = beatDuration * 4
-framesBetweenBars = 96
-frame = 0
-i = 0
+framesBetweenBars = FPS*barDuration
 
 def calculatePos(bar):
 	return (bar * framesBetweenBars) + OFFSET
 
 def printMarkerCode(bar):
 	print('    {')
-	print('        "comment": "' + str(bar) + ' - 3",')
+	print('        "comment": "' + str(bar) + ' - 1",')
 	print('        "pos": ' + str(calculatePos(bar)) + ',')
 	print('        "type": ' + str(MARKERTYPE))
 	print('    },')
 
+i = 0
 while (calculatePos(i) < SONGLENGTH):
 	printMarkerCode(i)
 	i = i + 1
